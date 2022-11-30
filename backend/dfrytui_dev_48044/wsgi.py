ba8fe901d7c1207f8bd8530473f314189044cb77
Crowdbotics <engineering@crowdbotics.com>
2022-11-30 17:14:35 +0000

    Initial commit.

---
+++ backend/dfrytui_dev_48044/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dfrytui_dev_48044 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dfrytui_dev_48044.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dfrytui-dev-48044",
+  url: "https://dfrytui-dev-48044.botics.co",
+  api: "https://dfrytui-dev-48044.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
